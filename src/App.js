da219a1ff23da7cafcfb652ea73bd8468b7d5eaf
Yerlan18 <66105164+Yerlan18@users.noreply.github.com>
2020-12-17 20:07:50 +0600

    product component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import react from 'react'
-function App() {
-  return (
-    <div className="grid-container">
-  <header>
-    <a href="/">React Shopping Cart</a>
-  </header>
-      <main>
-        Product list
-      </main>
-      <footer>
-        All right is reserved.
-      </footer>
-    </div>
-  );
+import Products from "./components/Components";
+import Hello from './Hello'
+
+
+
+class App extends react.Component {
+    constructor() {
+        super();
+        this.state = {
+            products: Hello.products,
+            size: "",
+            sort: ""
+        };
+    }
+
+    render() {
+        return (
+            <div className="grid-container">
+                <header>
+                    <a href="/">React Shopping Cart</a>
+                </header>
+                <main>
+                    <div className="content">
+                        <div className="main">
+                            <Products products={this.state.products}></Products>
+                        </div>
+                        <div className="sidebar">
+                            Cart Items
+                        </div>
+                    </div>
+                </main>
+                <footer>
+                    All right is reserved.
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
